6516d01ca12a3cd2332047ca5dc52cde083ef268
Kubrakov Oleg <kubrakov@tutu.ru>
2015-06-19 19:39:03 +0700

    Initial Commit

---
+++ Finance App for Travellers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Finance App for Travellers
+//
+//  Created by Oleg Kubrakov on 19/06/15.
+//  Copyright (c) 2015 Oleg Kubrakov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
