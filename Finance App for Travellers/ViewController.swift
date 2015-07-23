af83be16fd6fc72575fc70a9bed7de5aef1aa4b4
Kubrakov Oleg <kubrakov@tutu.ru>
2015-07-23 15:29:40 +0700

    tap tap

--- Finance App for Travellers/ViewController.swift
+++ Finance App for Travellers/ViewController.swift
@@ -11,12 +11,20 @@ import UIKit
 class ViewController: UIViewController {
 
     var currentCurrrency: Currency?
+	var amount:String = ""
     
     @IBOutlet weak var networkingIndicator: UIActivityIndicatorView!
-    
+	@IBOutlet weak var amountDisplayButton: UIButton!
+	@IBOutlet weak var number1: UIButton!
+	@IBOutlet weak var number2: UIButton!
+	
     override func viewDidLoad() {
         super.viewDidLoad()
-		navigationController?.setNavigationBarHidden(true, animated: false)
+		navigationController?.navigationBar.barTintColor = UIColor.whiteColor()
+		navigationController?.navigationBar.barStyle = .Black
+		
+		
+
         /*
         var alert: UIAlertView = UIAlertView(title: "Title", message: "Please wait...", delegate: nil, cancelButtonTitle: "Cancel");
         var loadingIndicator: UIActivityIndicatorView = UIActivityIndicatorView(frame: CGRectMake(50, 10, 37, 37)) as UIActivityIndicatorView
@@ -37,13 +45,104 @@ class ViewController: UIViewController {
 		//Networking().downloadCurrenciesDatabase({self.networkingIndicator.stopAnimating()})
     }
 
+	@IBAction func tapNumber1(sender: UIButton)
+	{
+		amount = amount + "1"
+		reloadAmountDisplay()
+	}
+
+	
+	@IBAction func tapNumber2(sender: UIButton) {
+		amount = amount + "2"
+		reloadAmountDisplay()
+	}
+	
+	
+	@IBAction func tapNumber3(sender: UIButton) {
+		amount = amount + "3"
+		reloadAmountDisplay()
+
+	}
+	
+	
+	@IBAction func tapNumber4(sender: UIButton) {
+		amount = amount + "4"
+		reloadAmountDisplay()
+
+	}
+	
+	
+	@IBAction func tapNumber5(sender: UIButton) {
+		amount = amount + "5"
+		reloadAmountDisplay()
+
+	}
+	
+	
+	@IBAction func tapNumber6(sender: UIButton) {
+		amount = amount + "6"
+		reloadAmountDisplay()
+
+	}
+	
+	
+	@IBAction func tapNumber7(sender: UIButton) {
+		amount = amount + "7"
+		reloadAmountDisplay()
+
+	}
+	
+	
+	@IBAction func tapNumber8(sender: UIButton) {
+		amount = amount + "8"
+		reloadAmountDisplay()
+
+	}
+	
+	
+	@IBAction func tapNumber9(sender: UIButton) {
+		amount = amount + "9"
+		reloadAmountDisplay()
+
+	}
+	
+	
+	@IBAction func tapNumberDiv(sender: UIButton) {
+		amount = amount + "."
+		reloadAmountDisplay()
+
+	}
+	
+	
+	@IBAction func tapNumber0(sender: UIButton) {
+		amount = amount + "0"
+		reloadAmountDisplay()
+
+	}
+	
+	
+	@IBAction func tapNumberDel(sender: UIButton) {
+		amount = amount.substringToIndex(amount.endIndex.predecessor())
+		reloadAmountDisplay()
+
+	}
+	
+	@IBAction func tapSaveTransaction(sender: AnyObject) {
+	}
+	
+	
+	func reloadAmountDisplay()
+	{
+		amountDisplayButton.titleLabel?.adjustsFontSizeToFitWidth = true
+		amountDisplayButton.titleLabel?.text = amount
+	}
+	
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
     @IBAction func selectedCurrency(segue:UIStoryboardSegue) {
-		navigationController?.setNavigationBarHidden(true, animated: true)
         if let currenciesTableViewController = segue.sourceViewController as? CurrenciesTableViewController,
             selectedCurrency = currenciesTableViewController.selectedCurrency {
 
@@ -54,13 +153,11 @@ class ViewController: UIViewController {
 	
 	override func viewDidAppear(animated: Bool)
 	{
-		navigationController?.setNavigationBarHidden(true, animated: true)
 		super.viewDidAppear(animated)
 	}
 	
 	override func viewDidDisappear(animated: Bool)
 	{
-		navigationController?.setNavigationBarHidden(false, animated: true)
 		super.viewDidDisappear(animated)
 	}
 	
