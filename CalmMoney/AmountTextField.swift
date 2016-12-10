61cb5065d80576664f803d2c76af6a4ffcb1a958
Oleg Kubrakov <surfingcat@surfingcat.local>
2016-12-10 23:52:37 +0800

    text field text constraints

--- CalmMoney/AmountTextField.swift
+++ CalmMoney/AmountTextField.swift
@@ -8,8 +8,17 @@
 
 import UIKit
 
-class AmountTextField: UITextField {
+class AmountTextField: UITextField, UITextFieldDelegate {
 
+	private var characterLimit: Int?
+	
+	
+	required init?(coder aDecoder: NSCoder) {
+		super.init(coder: aDecoder)
+		delegate = self
+	}
+	
+	
     // Only override drawRect: if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
@@ -46,5 +55,34 @@ class AmountTextField: UITextField {
     }
 	
 	var correspondingCurrency: HandsOnCurrency?
+	
+	
+	@IBInspectable var maxLength: Int {
+		get {
+			guard let length = characterLimit else {
+				return Int.max
+			}
+			return length
+		}
+		set {
+			characterLimit = newValue
+		}
+	}
+	
+	
+	func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+  
+		// 2
+		guard string.characters.count > 0 else {
+			return true
+		}
+		
+		// 3
+		let currentText = textField.text ?? ""
+		// 4
+		let prospectiveText = (currentText as NSString).replacingCharacters(in: range, with: string)
+		// 5
+		return prospectiveText.characters.count <= maxLength
+	}
 
 }
